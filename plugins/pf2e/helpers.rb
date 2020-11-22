b6d8a033bebea8c16192074d44b0740213154076
Emblem of Ea MU <emblemofeamush@gmail.com>
2020-11-22 13:37:23 +0000

    Saving before upgrade.

--- plugins/pf2e/helpers.rb
+++ plugins/pf2e/helpers.rb
@@ -1,18 +1,5 @@
 module AresMUSH
   module Pf2e
 
-    PROF_LEVEL = {
-        untrained=>0,
-        trained=>2,
-        expert=>4,
-        master=>6,
-        legendary=>8
-      }
-
-    def get_prof_bonus(p=untrained)
-      levels = AresMUSH::Pf2e::PROF_LEVEL
-      bonus = levels[p]
-      return #{bonus}
-    end
   end
 end
