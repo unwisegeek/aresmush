f1caee946594376ce3c6a0befbeea8fb0ea14b32
Alison W <68201554+mordenkainensmom@users.noreply.github.com>
2020-11-27 08:26:11 -0500

    get_ability_mod, get_prof_bonus

--- plugins/pf2e/helpers.rb
+++ plugins/pf2e/helpers.rb
@@ -1,5 +1,21 @@
 module AresMUSH
   module Pf2e
 
+    def find_ability_score(char,ability)
+    end
+
+    def find_prof_level(char, ability)
+    end
+
+    def get_ability_mod(score)
+      mod = (score - 10) / 2
+    end
+
+    def get_prof_bonus(p=:untrained)
+      levels = { untrained: 0, trained: 2, expert: 4, master: 6, legendary: 8 }
+      p = p.to_sym
+      bonus = levels[p]
+    end
+
   end
 end
