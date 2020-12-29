1581d5ac844f018cb7111f422312532336edfc75
Emblem of Ea MU <emblemofeamush@gmail.com>
2020-12-29 18:55:25 +0000

    Saving before upgrade.

--- plugins/alttracker/commands/register_alt.rb
+++ plugins/alttracker/commands/register_alt.rb
@@ -17,29 +17,14 @@ module AresMUSH
       end
 
       def handle
-        max_alts = Global.read_config('alttracker','max_alts_allowed')
-
-        ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-
         valid_email = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
         max_alts = Global.read_config('alttracker','max_alts_allowed')
 
-          if !player
-            client.emit_failure t('alttracker.not_registered', :name => self.name)
-            return nil
-          elsif player.characters.size >= max_alts
-            client.emit_failure t('alttracker.max_alts_exceeded', :max_alts => max_alts)
-            return nil
-          elsif player.banned
-            client.emit_failure t('alttracker.player_banned')
-            return nil
-          elsif !(self.codeword == player.codeword)
-            client.emit_failure t('alttracker.invalid_codeword')
-            return nil
-          else
-            enactor.update(player: player)
-            client.emit_success t('alttracker.register_ok')
-          end
+        if self.target =~ valid_email
+          player = AltTracker.find_player_by_email(self.target)
+        else
+          player = Character.find_one_by_name(self.target)&.player
+        end
 
         if !player
           client.emit_failure t('alttracker.not_registered', :name => self.target)
