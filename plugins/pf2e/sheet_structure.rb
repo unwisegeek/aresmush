3f80776b9833045027e31fd3c6e62a2dfe7e3d47
Alison W <68201554+mordenkainensmom@users.noreply.github.com>
2021-01-09 11:41:08 -0500

    Help files and basic structure for PF2.

---
+++ plugins/pf2e/sheet_structure.rb
@@ -0,0 +1,21 @@
+module AresMUSH
+  module Pf2e
+    class Pf2eSheet < Ohm::Model
+      include ObjectModel
+
+      attribute :pf2_ancestry
+      attribute :pf2_background
+      attribute :pf2_heritage
+      attribute :pf2_class
+      attribute :pf2_level
+      attribute :pf2_faith
+      attribute :pf2_deity
+      attribute :pf2_alignment
+
+      reference :char, "AresMUSH::Character"
+    end
+
+  class Character
+    reference :pf2sheet, "AresMUSH::Pf2e::Pf2eSheet"
+  end
+end
