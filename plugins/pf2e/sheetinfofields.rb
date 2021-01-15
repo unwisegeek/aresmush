40f3aa67513f5e0503792d616bf6524076e4ec35
Alison W <68201554+mordenkainensmom@users.noreply.github.com>
2021-01-15 17:29:42 -0500

    Beginnings of sheet display.

---
+++ plugins/pf2e/sheetinfofields.rb
@@ -0,0 +1,40 @@
+module AresMUSH
+  module SheetInfoFields
+
+    def char_name
+      char.name
+    end
+
+    def ancestry
+      sheet.pf2_ancestry
+    end
+
+    def background
+      sheet.pf2_background
+    end
+
+    def class
+      sheet.pf2_class
+    end
+
+    def deity
+      sheet.pf2_deity
+    end
+
+    def faith
+      sheet.pf2_faith
+    end
+
+    def heritage
+      sheet.pf2_heritage
+    end
+
+    def level
+      sheet.pf2_level
+    end
+
+    def size
+      sheet.pf2_size
+    end
+  end
+end
