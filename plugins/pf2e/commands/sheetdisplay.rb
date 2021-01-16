69e2cfd539202e00ef26b2b516c38af3bc96ec11
Alison W <68201554+mordenkainensmom@users.noreply.github.com>
2021-01-16 10:08:01 -0500

    Janky conditional, fix?

--- plugins/pf2e/commands/sheetdisplay.rb
+++ plugins/pf2e/commands/sheetdisplay.rb
@@ -20,10 +20,10 @@ module AresMUSH
 
       def handle
         char = self.target ? ClassTargetFinder.find(self.target, Character, enactor) : enactor
-        return && client.emit_failure t('pf2e.char_not_found') if !char
+        client.emit_failure t('pf2e.char_not_found') && return ("Not found.") if !char
 
         sheet = char.pf2sheet
-        return && client.emit_failure t('pf2e.sheet_not_found') if !sheet
+        client.emit_failure t('pf2e.sheet_not_found') && return ("Not found.") if !sheet
 
         case self.section
         when "all"
