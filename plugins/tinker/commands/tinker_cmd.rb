9a6f24704307a77a6aa0222b13f6d82a921be474
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-02-07 16:48:25 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,35 +9,29 @@ module AresMUSH
       end
       
       def handle
-        
-        char = Character.find_one_by_name('Landtest')
-
-        base_info = char.pf2_base_info
-        ancestry = base_info['ancestry']
-        heritage = base_info['heritage']
-        background = base_info['background']
-        charclass = base_info['charclass']
-        subclass = base_info['specialize']
-
-        ancestry_info = ancestry.blank? ? {} : Global.read_config('pf2e_ancestry', ancestry)
-        heritage_info = heritage.blank? ? {} : Global.read_config('pf2e_heritage', heritage)
-        background_info = background.blank? ? {} : Global.read_config('pf2e_background', background)
-        charclass_info = charclass.blank? ? {} : Global.read_config('pf2e_class', charclass)
-        faith_info = char.pf2_faith
-        
-        a_traits = ancestry_info["traits"] ? ancestry_info["traits"] : []
-        h_traits = heritage_info["traits"] ? heritage_info["traits"] : []
-        c_traits = charclass_info ? [ charclass.downcase ] : []
-
-        traits = a_traits + h_traits + c_traits.uniq.sort
-        
-        client.emit a_traits
-        client.emit h_traits
-        client.emit c_traits
-        client.emit traits
-        client.emit charclass.downcase
-        
+      
+          charclass = "Alchemist"
+          align = "NG"
+          deity = nil
             
+          class_alignments = Global.read_config('pf2e_class', charclass, 'allowed_alignments')
+          requires_deity = Global.read_config('pf2e_class', charclass, 'check_deity')
+          deity_alignments = Global.read_config('pf2e_deities', deity, 'allowed_alignments')
+    
+          if !class_alignments
+            class_alignments = Global.read_config('pf2e', 'allowed_alignments')
+          end
+    
+          if requires_deity && (!deity || deity.blank?)
+            error = t('pf2e.class_requires_deity')
+          elsif requires_deity
+            error = class_alignments & deity_alignments.include?(align) ? nil : t('pf2e.class_deity_mismatch')
+          else
+            error = class_alignments.include?(align) ? nil : t('pf2e.class_mismatch')
+          end
+    
+          client.emit class_alignments
+          client.emit error ? error : "None"
       end
 
     end
