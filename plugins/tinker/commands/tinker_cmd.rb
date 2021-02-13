9ee85a5fbefa8fab8e52b2175b6fd9a45c13751e
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-02-13 21:15:27 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,29 +9,10 @@ module AresMUSH
       end
       
       def handle
-      
-          charclass = "Alchemist"
-          align = "NG"
-          deity = nil
-            
-          class_alignments = Global.read_config('pf2e_class', charclass, 'allowed_alignments')
-          requires_deity = Global.read_config('pf2e_class', charclass, 'check_deity')
-          deity_alignments = Global.read_config('pf2e_deities', deity, 'allowed_alignments')
-    
-          if !class_alignments
-            class_alignments = Global.read_config('pf2e', 'allowed_alignments')
-          end
-    
-          if requires_deity && (!deity || deity.blank?)
-            error = t('pf2e.class_requires_deity')
-          elsif requires_deity
-            error = class_alignments & deity_alignments.include?(align) ? nil : t('pf2e.class_deity_mismatch')
-          else
-            error = class_alignments.include?(align) ? nil : t('pf2e.class_mismatch')
-          end
-    
-          client.emit class_alignments
-          client.emit error ? error : "None"
+        list = "Not an array"
+        test = list.is_a?(Array) ? list.join(" and ") : list
+        
+        client.emit  test
       end
 
     end
