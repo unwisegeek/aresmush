7caf9f2245c511410eb81b73671222cf1abab779
Alison W <68201554+mordenkainensmom@users.noreply.github.com>
2021-02-23 15:58:04 -0500

    commit, sheet/show, cg/review updates

--- plugins/pf2e/templates/cg_review.rb
+++ plugins/pf2e/templates/cg_review.rb
@@ -23,6 +23,12 @@ module AresMUSH
         @charclass_info = @charclass.blank? ? {} : Global.read_config('pf2e_class', @charclass)
         @faith_info = @char.pf2_faith
 
+        @baseinfolock = @char.pf2_baseinfo_locked
+        @abil_lock = @char.pf2_abilities_locked
+        @class_features_info = @charclass_info['chargen']
+        @to_assign = @char.pf2_to_assign
+        @boosts = @char.pf2_boosts_working
+
         super File.dirname(__FILE__) + "/cg_review.erb"
       end
 
@@ -90,13 +96,26 @@ module AresMUSH
         traits = a_traits + h_traits + c_traits.uniq.sort
       end
 
+      def free_boosts
+        @baseinfolock ? @to_assign['open boost'] : 4
+      end
+
       def ancestry_boosts
-        list = @ancestry_info["abl_boosts"] ? @ancestry_info["abl_boosts"] : "?"
-        list.is_a?(Array) ? list.join(" and ") : list
+        if @baseinfolock
+          list = @boosts['ancestry']
+          list.sort.join(", ")
+        else
+          list = @ancestry_info["abl_boosts"] ? @ancestry_info["abl_boosts"] : "?"
+          list.is_a?(Array) ? list.join(" and ") : list
+        end
       end
 
       def free_ancestry_boosts
-        @ancestry_info["abl_boosts_open"] ? @ancestry_info["abl_boosts_open"] : 0
+        if @baseinfolock
+          @to_assign['open anboost'] ? @to_assign['open anboost'] : 0
+        else
+          @ancestry_info["abl_boosts_open"] ? @ancestry_info["abl_boosts_open"] : 0
+        end
       end
 
       def ancestry_flaw
@@ -104,32 +123,96 @@ module AresMUSH
       end
 
       def background_boosts
-        list = @background_info["req_abl_boosts"] ? @background_info["req_abl_boosts"] : []
-        list.empty? ? "None required" : list.join(" or ")
+        if @baseinfolock
+          list = @boosts['background']
+          list.sort.join(", ")
+        else
+          list = @background_info["req_abl_boosts"] ? @background_info["req_abl_boosts"] : []
+          list.empty? ? "None required" : list.join(" or ")
+        end
       end
 
       def free_bg_boosts
-        @background_info["abl_boosts_open"] ? @background_info["abl_boosts_open"] : 0
+        if @baseinfolock
+          @to_assign['open bgboost'] ? @to_assign['open bgboost'] : 0
+        else
+          @background_info["abl_boosts_open"] ? @background_info["abl_boosts_open"] : 0
+        end
       end
 
       def charclass_boosts
-        @charclass_info["key_abil"] ? @charclass_info["key_abil"].join(" or ") : "Class not set."
+        if @baseinfolock
+          list = @boosts['charclass']
+          list.sort.join(", ")
+        else
+          @charclass_info["key_abil"] ? @charclass_info["key_abil"].join(" or ") : "Class not set."
+        end
+      end
+
+      def con_mod
+        if @abil_lock
+          con_mod = Pf2e.get_ability_mod(Pf2eAbilities.get_ability_score(@char, "Constitution"))
+        else
+          con_mod = "CON Mod"
+        end
+      end
+
+      def int_mod
+        if @abil_lock
+          int_mod = Pf2e.get_ability_mod(Pf2eAbilities.get_ability_score(@char, "Intelligence"))
+        else
+          int_mod = "INT Mod"
+        end
       end
 
       def specials
-        ainfo = @ancestry_info["special"] ? @ancestry_info["special"] : []
-        hinfo = @heritage_info["special"] ? @heritage_info["special"] : []
-        binfo = @background_info["special"] ? @background_info["special"] : []
-        specials = ainfo + hinfo + binfo.flatten
-        if specials.include?("Low-Light Vision") && @heritage_info["change_vision"]
-          specials = specials - [ "Low-Light Vision" ] + [ "Darkvision" ]
+        if @baseinfolock
+          specials = @char.pf2_special
+        else
+          ainfo = @ancestry_info["special"] ? @ancestry_info["special"] : []
+          hinfo = @heritage_info["special"] ? @heritage_info["special"] : []
+          binfo = @background_info["special"] ? @background_info["special"] : []
+          specials = ainfo + hinfo + binfo.flatten
+          if specials.include?("Low-Light Vision") && @heritage_info["change_vision"]
+            specials = specials - [ "Low-Light Vision" ] + [ "Darkvision" ]
+          end
         end
         specials.empty? ? "No special abilities or senses." : specials.sort.join(", ")
       end
 
+      def languages
+        @ancestry_info ? @ancestry_info['languages'].sort.join(", ") : "Tradespeak"
+      end
+
+      def bg_skills
+        @to_assign['bgskill'].join(" or ") if @baseinfolock
+      end
+
       def messages
-        msgs = Pf2e.chargen_messages(@ancestry, @heritage, @background, @charclass, @subclass, @char.pf2_faith)
-        msgs ? msgs : t('pf2e.cg_options_ok')
+        if @abil_lock
+          t('pf2e.cg_and_abil_lock_ok')
+        elsif @baseinfolock
+          msgs = Pf2eAbilities.abilities_messages(@char)
+          msgs ? msgs : t('pf2e.abil_options_ok')
+        else
+          msgs = Pf2e.chargen_messages(@ancestry, @heritage, @background, @charclass, @subclass, @char.pf2_faith)
+          msgs ? msgs : t('pf2e.cg_options_ok')
+        end
+      end
+
+      def format_abil(name, value)
+        fname = "#{item_color}#{name}%xn:"
+        linebreak = i % 3 == 1 ? "" : "%r"
+        fmod = "(#{Pf2eAbilities.get_ability_mod(value)})"
+        "#{linebreak}#{left(fname, 10)}: #{left(value, 3)} #{left(fmod, 13)}"
+      end
+
+      def format_skill(name, value)
+        # This isn't done yet
+        name = "#{item_color}#{name}:%xn"
+        linebreak = i % 2 == 1 ? "" : "%r"
+        value
+        "#{linebreak}#{left(name, 21)} #{left(s.print_rating, 8)} #{left(rating_text, 10)}"
       end
 
     end
