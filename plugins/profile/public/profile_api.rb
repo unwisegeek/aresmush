95650b2209b541fb2c4c92cbd9f3129602220f03
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-02-24 23:12:00 +0000

    Saving before upgrade.

--- plugins/profile/public/profile_api.rb
+++ plugins/profile/public/profile_api.rb
@@ -40,12 +40,18 @@ module AresMUSH
       when 'ancestry'
         char.is_admin? ? "" : char.pf2_base_info['ancestry']
 
+      when 'heritage'
+        char.is_admin? ? "" : char.pf2_base_info['heritage']
+
       when 'charclass'
         char.is_admin? ? "" : char.pf2_base_info['charclass']
 
       when 'level'
-        char.is_admin? ? "": char.pf2_level
+        char.is_admin ? "" : char.pf2_level
 
+      when 'tier'
+        level = char.pf2_level
+        char.is_admin? ? '-' : Pf2e.get_level_tier(level) 
       else
         nil
       end
