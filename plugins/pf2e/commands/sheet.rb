c316f5bd98e2ecd77eae9988c6b3bcbf33702b2a
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-03-14 14:07:48 +0000

    Saving before upgrade.

--- plugins/pf2e/commands/sheet.rb
+++ plugins/pf2e/commands/sheet.rb
@@ -19,6 +19,11 @@ module AresMUSH
       end
 
       def handle
+        if !char.pf2_baseinfo_locked
+          client.emit_failure t('pf2e.no_sheet_yet')
+          return
+        end
+
         char = self.target ? Character.find_one_by_name(self.target) : enactor
 
         case self.section
