fcca8a870c330eda2b91e62507c0fe486173873d
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-03-20 18:53:36 +0000

    Saving before upgrade.

--- plugins/pf2e/commands/sheet.rb
+++ plugins/pf2e/commands/sheet.rb
@@ -21,8 +21,20 @@ module AresMUSH
       def handle
         char = self.target ? Character.find_one_by_name(self.target) : enactor
 
-        case self.section
-        when "all", "info", "ability", "skills", "feats", "combat"
+        if !char
+          client.emit_failure t('pf2e.char_not_found')
+          return
+        elsif char.is_admin?
+          client.emit_failure t('pf2e.admin_no_sheet')
+          return
+        elsif !char.pf2_baseinfo_locked
+          client.emit_failure t('pf2e.no_sheet_yet')
+          return
+        end
+
+        valid_sections = %w{all info ability skills feats combat features languages}
+
+        if valid_sections.include? self.section
           template = Pf2eSheetTemplate.new(char, self.section, client, char.pf2_base_info, char.pf2_faith)
         else
           client.emit_failure t('pf2e.bad_section', :section => self.section)
