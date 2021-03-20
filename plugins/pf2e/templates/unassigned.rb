7a46eeadeda94c33202a167eee0fdde83201449d
Alison W <68201554+mordenkainensmom@users.noreply.github.com>
2021-03-20 14:41:30 -0400

    Unassigned command

---
+++ plugins/pf2e/templates/unassigned.rb
@@ -0,0 +1,39 @@
+module AresMUSH
+  module Pf2e
+
+    class Pf2eUnassignedTemplate < ErbTemplateRenderer
+      include CommonTemplateFields
+
+      attr_accessor :char, :to_assign
+
+      def initialize(char, to_assign)
+        @char = char
+        @to_assign = to_assign
+
+        super File.dirname(__FILE__) + "/unassigned.erb"
+      end
+
+      def title
+        t('pf2e.unassigned_title', :name => @char.name)
+      end
+
+      def to_assign
+        list = []
+
+        @to_assign.each_pair do |key, value|
+          list << format_option(key, value)
+        end
+
+        list
+      end
+
+      def format_option(element, options)
+        title_e = element.split.each { |word| word.capitalize }.join
+        fmt_e = "#{title_e} : "
+        fmt_o = options.join(" or ")
+
+        "%b%b%b#{item_color}#{fmt_e}#{fmt_o}%r"
+      end
+    end
+  end
+end
