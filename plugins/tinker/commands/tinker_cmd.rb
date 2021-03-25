973e341de76a715034a45521a426607ae5ec7949
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-03-25 17:29:34 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -10,22 +10,7 @@ module AresMUSH
       
       def handle
       
-        roll_list = %w{1d6 5}
-        dice_pattern = /([0-9]+)d[0-9]+/i
-      
-        result = []
-        roll_list.map do |e|
-          if e =~ dice_pattern
-            dice = e.gsub("d"," ").split
-            amount = dice[0].to_i > 0 ? dice[0].to_i : 1
-            sides = dice[1].to_i
-            result << Pf2e.roll_dice(amount, sides)
-          elsif e.to_i == 0
-            result << Pf2e.get_keyword_value(enactor, e)
-          else
-            result << e.to_i
-          end
-        end
+        result = 4.clamp(0,3)
         
         client.emit result
     
