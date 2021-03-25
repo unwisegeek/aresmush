d012c9e5d6ff2585eb2794e5a0e2148142d19115
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-03-25 13:00:04 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,52 +9,26 @@ module AresMUSH
       end
       
       def handle
-        char = AresMUSH::Character.find_one_by_name('Landtest')
-        
-        base_info = char.pf2_base_info
-        faith = char.pf2_faith
-        
-        charclass = base_info['charclass']
-        subclass = base_info['specialize']
-        deity = faith['deity']
-        align = faith['alignment']
-        
-        client.emit "Charclass = " + charclass
-        client.emit "Subclass = " + subclass 
-        client.emit "Deity = " + deity  
-        client.emit "Alignment = " + align
       
-        all_align = Global.read_config('pf2e','allowed_alignments')
-        subclass_align = Global.read_config('pf2e_specialty', subclass, 'allowed_alignments')
-        class_align = Global.read_config('pf2e_class', charclass, 'allowed_alignments')
-        requires_deity = Global.read_config('pf2e_class', charclass, 'use_deity')
-        deity_alignments = Global.read_config('pf2e_deities', deity, 'allowed_alignments')
-
-        calign = class_align ? class_align : all_align
-        salign = subclass_align ? subclass_align : all_align
-
-        alignments = calign & salign
-        
-        client.emit alignments
-        client.emit deity_alignments
-        
-
-        if requires_deity && (!deity || deity.blank?)
-            error = t('pf2e.class_requires_deity')
-        elsif requires_deity
-            dalign = alignments & deity_alignments
-            
-            client.emit dalign
-            
-            error = dalign.include?(align) ?
-                    nil :
-                    t('pf2e.class_deity_mismatch')
-        else
-            error = alignments.include?(align) ? nil : t('pf2e.class_mismatch')
+        roll_list = %w{1d6 5}
+        dice_pattern = /([0-9]+)d[0-9]+/i
+      
+        result = []
+        roll_list.map do |e|
+          if e =~ dice_pattern
+            dice = e.gsub("d"," ").split
+            amount = dice[0].to_i > 0 ? dice[0].to_i : 1
+            sides = dice[1].to_i
+            result << Pf2e.roll_dice(amount, sides)
+          elsif e.to_i == 0
+            result << Pf2e.get_keyword_value(enactor, e)
+          else
+            result << e.to_i
+          end
         end
         
-        client.emit error
-        
+        client.emit result
+    
       end
 
     end
