3d9586761d575b52f0be0b404d6413aa710ccf3c
Emblem of Ea MU <emblemofeamush@gmail.com>
2021-12-25 19:10:26 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -3,28 +3,19 @@ module AresMUSH
     class TinkerCmd
       include CommandHandler
       
-      def handle
-      
-        char = Character.find_one_by_name("Testchar")
-      
-        boosts = char.pf2_boosts_working
-        
-        scores = {}
-        char.abilities.each do |a|
-            k = a.name
-            v = a.base_val
-            scores[k] = v
-        end
-        
-        score_chk = boosts.values.flatten
-        
-        client.emit "Char: #{char}"
-        client.emit "Boosts: #{boosts}"
-        client.emit "Scores: #{scores}"
-        client.emit "Score-chk: #{score_chk}"
-        
+      def check_can_manage
+        return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
+        return nil
       end
       
+      def handle
+        char = Character.find_one_by_name("Testchar")
+
+        strength = Pf2e.find_character_ability(char, 'ability', "Strength")
+        
+        client.emit "Strength: #{strength.name}"
+      end
+
     end
   end
 end
