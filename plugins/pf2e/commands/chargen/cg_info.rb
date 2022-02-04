9e0adc67fd1de1d1f702b935d38d021740ff77cd
Emblem of Ea MU <emblemofeamush@gmail.com>
2022-02-04 17:09:41 +0000

    Saving before upgrade.

--- plugins/pf2e/commands/chargen/cg_info.rb
+++ plugins/pf2e/commands/chargen/cg_info.rb
@@ -52,9 +52,9 @@ module AresMUSH
           options = Global.read_config('pf2e_deities').keys.sort
         when 'align', 'alignment'
           all_align = Global.read_config('pf2e','allowed_alignments')
-          subclass_align = subclass.blank? all_align : Global.read_config('pf2e_specialty', subclass, 'allowed_alignments')
-          class_align = charclass.blank? all_align : Global.read_config('pf2e_class', charclass, 'allowed_alignments')
-          deity_align = deity.blank? all_align : Global.read_config('pf2e_deities', deity, 'allowed_alignments')
+          subclass_align = subclass.blank? ? all_align : Global.read_config('pf2e_specialty', subclass, 'allowed_alignments')
+          class_align = charclass.blank? ? all_align : Global.read_config('pf2e_class', charclass, 'allowed_alignments')
+          deity_align = deity.blank? ? all_align : Global.read_config('pf2e_deities', deity, 'allowed_alignments')
 
           class_align = all_align if !class_align
           subclass_align = all_align if !subclass_align
@@ -69,7 +69,7 @@ module AresMUSH
 
         client.emit t('pf2e.cg_info',
           :element=>"%x24#{self.element}%xn",
-          :options=>"#{item_color}#{options.join(", ")}""
+          :options=>"#{item_color}#{options.join(", ")}"
         )
       end
 
