53ac73d562424dc7c3b01ea78ec649110fc45eed
Emblem of Ea MU <emblemofeamush@gmail.com>
2022-02-11 19:26:53 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,30 +9,44 @@ module AresMUSH
       end
       
       def handle
-        char = Character.find_one_by_name("Karasu")
+        char = Character.find_one_by_name("Testchar")
           
-        base_info = char.pf2_base_info
-          
-        subclass_option = base_info['specialize_info']
-        charclass = base_info['charclass']
-        subclass = base_info['specialize']
+        to_assign = {}
         
-        subclass_info = Global.read_config('pf2e_specialty', charclass, subclass)
-          
-        subclass_option_info = subclass_option.blank? ?
-                               nil :
-                               subclass_info['choose']['options'][subclass_option]
-                               
-        subclassopt_features_info = subclass_option_info ? subclass_option_info['chargen'] : {}  
+        bg_lores = [ "Warfare Lore" ]
+
+        if bg_lores.size > 1
+          client.emit_ooc t('pf2e.multiple_options', :element=>"lore")
+          to_assign['bglore'] = bg_lores
+          bg_lores = []
+        elsif bg_lores.size == 0
+          bg_lores = []
+          client.emit_ooc t('pf2e.bg_no_options', :element => "lores")
+        end
+
+        # No class or specialty right now grants lores, this is left in in case they do later.
+        #
+        # class_lores = class_features_info["lores"] ? class_features_info["lores"] : []
+        # subclass_lores = subclass_features_info["lores"] ? subclass_features_info["lores"] : []
+        # lores = bg_lores + class_lores + subclass_lores
+
+        lores = bg_lores
+
+        unique_lores = lores.uniq
+
+        if !(unique_lores.empty?)
+          unique_lores.each do |lore|
+
+            Pf2eLores.create_lore_for_char(lore, char, true)
+          end
+        end
         
-        subclassopt_skills = subclassopt_features_info['skills']
+        client.emit lores
+        client.emit unique_lores
+        client.emit to_assign
+        client.emit char.lores.each { |lore| lore.name }.join(", ")
         
-        client.emit base_info
-        client.emit subclass_option
-        client.emit subclass_option_info
-        client.emit subclassopt_features_info
-        client.emit subclassopt_skills
-        
+        client.emit lores.uniq
       end
 
     end
