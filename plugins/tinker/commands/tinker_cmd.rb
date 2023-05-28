aab7e5fc89691b9a543ac622660237b8ac5279d8
Emblem of Ea MU <emblemofeamush@gmail.com>
2023-05-28 19:31:57 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,12 +9,11 @@ module AresMUSH
       end
       
       def handle
-        feat_list = [ "Wary Disarmament", "Diehard", "Vibrant Display", "Adopted Ancestry" ]
-
-        @details = Global.read_config('pf2e_feats').keep_if { |k,v| feat_list.include? k }
+        char = Character.find_one_by_name("Testchar")
         
-        client.emit "Feat List: #{feat_list.sort.join(", ")}"
-        client.emit "Feat Details Hash: #{@details}"
+        feat_list = char.pf2_feats.values
+        
+        client.emit feat_list
         
         
       end
