787759d3df808d17fd71fe3be3eefdf6841ac696
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2023-05-28 13:44:18 -0700

    Add in admin/other view option

--- plugins/pf2e/commands/feats/feat_info.rb
+++ plugins/pf2e/commands/feats/feat_info.rb
@@ -4,9 +4,23 @@ module AresMUSH
     class PF2FeatInfoCmd
       include CommandHandler
 
+      def parse_args
+        self.target = trim_arg(cmd.args)
+      end
+
+      def check_can_view
+        return nil if !self.target
+        return nil if Global.read_config('pf2e','open_sheets')
+        return nil if enactor.has_permission?("view_sheets")
+
+        return t('dispatcher.not_allowed')
+      end
+
       def handle
+
+        char = self.target ? Character.find_one_by_name(self.target) : enactor
         
-        feat_list = Pf2e.generate_list_details(enactor.pf2_feats.values.flatten)
+        feat_list = Pf2e.generate_list_details(char.pf2_feats.values.flatten)
 
         if feat_list.empty?
           client.emit_failure t('pf2e.nothing_to_display', :elements => "feats")
