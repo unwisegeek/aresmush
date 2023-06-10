a6bed8d677b82f12340a84b3bbe5c28852394be1
Emblem of Ea MU <emblemofeamush@gmail.com>
2023-06-10 21:59:46 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,12 +9,17 @@ module AresMUSH
       end
       
       def handle
+      
         
         char = Character.find_one_by_name("Testchar")
         
+        char.pf2_invested_list = []
+        
+        char.save
+        
         item_list = Pf2egear.items_in_inventory(char.magic_items.to_a)
         
-        client.emit item_list
+        client.emit item_list.first
         
       end
 
