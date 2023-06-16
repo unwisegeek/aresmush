4fcee7aa832d4da0bb081e561eb670fb2492c232
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2023-06-16 09:05:22 -0700

    Behave.

--- plugins/pf2e/helpers/utils.rb
+++ plugins/pf2e/helpers/utils.rb
@@ -178,11 +178,13 @@ module AresMUSH
       succ_mod = 0
       whirldice = ""
 
-      if list[0].flatten == '1d20'
+      if list[0] == '1d20'
+        whirldice = "I GOT INVOKED"
+
         succ_mod = 1 if result[0] == 20
         succ_mod = -1 if result[0] == 1
 
-        whirldice = " %x200*** YOU'VE BEEN WHIRLDICED! ***%xn" if result[0] == 1
+        # whirldice = " %x200*** YOU'VE BEEN WHIRLDICED! ***%xn" if result[0] == 1
       end
 
       success_case = (scase + succ_mod).clamp(0,3)
