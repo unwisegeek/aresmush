7d7ee621ca8b6a34ee12f711fbb16a2b5b6c7dc4
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-01-20 12:06:05 -0800

    I should not be coding today 💤

--- plugins/pf2emagic/helpers/cg_magic_helpers.rb
+++ plugins/pf2emagic/helpers/cg_magic_helpers.rb
@@ -183,8 +183,8 @@ module AresMUSH
       prepared = Global.read_config('pf2e_magic', 'prepared_casters')
       spont = Global.read_config('pf2e_magic', 'spontaneous_casters')
 
-      return 'prepared' if prepared.include charclass
-      return 'spontaneous' if spont.include charclass
+      return 'prepared' if prepared.include? charclass
+      return 'spontaneous' if spont.include? charclass
       return nil
 
     end
