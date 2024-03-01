be5c1bb8f60c2f9dbe6b107f350572dc0907cbaa
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-02-29 17:53:09 -0800

    99 little bugs

--- plugins/pf2e/commands/feats/feat_set.rb
+++ plugins/pf2e/commands/feats/feat_set.rb
@@ -4,14 +4,14 @@ module AresMUSH
       include CommandHandler
 
       attr_accessor :feat_type, :feat_name, :feat_details
-      
+
       def parse_args
         args = cmd.parse_args(ArgParser.arg1_equals_arg2)
 
         self.feat_type = downcase_arg(args.arg1)
-        self.feat_name = titlecase_arg(args.arg2)
+        self.feat_name = upcase_arg(args.arg2)
       end
-      
+
       def required_args
         [ self.feat_type, self.feat_name ]
       end
@@ -50,7 +50,7 @@ module AresMUSH
 
         ##### VALIDATION SECTION START #####
 
-        # Does the enactor already have this feat? 
+        # Does the enactor already have this feat?
 
         feat_list = enactor.pf2_feats
 
@@ -59,7 +59,7 @@ module AresMUSH
           return nil
         end
 
-        # Does the enactor have one of the requested feat type free to select? 
+        # Does the enactor have one of the requested feat type free to select?
 
         to_assign = enactor.pf2_to_assign
 
@@ -92,10 +92,10 @@ module AresMUSH
 
         ##### VALIDATION SECTION END #####
 
-        # Do I need to replace the feat in the list or add to the list? 
+        # Do I need to replace the feat in the list or add to the list?
 
         replace = old_value.match?('unassigned') ? false : true
-        
+
         sublist = feat_list[self.feat_type]
 
         if replace
@@ -103,7 +103,7 @@ module AresMUSH
 
           sublist.delete_at index if index
 
-          # If the old feat had any magic stats, those need to be scrubbed. 
+          # If the old feat had any magic stats, those need to be scrubbed.
 
           old_fd = Pf2e.get_feat_details(old_value)
 
@@ -116,7 +116,7 @@ module AresMUSH
             PF2Magic.update_magic(enactor, charclass, old_magic_stats, client, true)
           end
 
-          # A very few feats modify reagents. Handle these. 
+          # A very few feats modify reagents. Handle these.
           reagents = old_fd['reagents']
 
           if reagents
@@ -134,7 +134,7 @@ module AresMUSH
 
         client.emit_success t('pf2e.feat_set_ok', :name => self.feat_name, :type => self.feat_type)
 
-        # Some feats grant magic of some sort. Handle here. 
+        # Some feats grant magic of some sort. Handle here.
 
         magic_stats = self.feat_details['magic_stats']
 
@@ -143,21 +143,21 @@ module AresMUSH
         if magic_stats
           client.emit_ooc 'This feat has magic details.'
 
-          # If it is a dedication feat, the key should indicate the character 
+          # If it is a dedication feat, the key should indicate the character
           # class to be used. Otherwise, the key is the name of the feat.
 
           charclass = use_diff_charclass ? use_diff_charclass : enactor.pf2_base_info['charclass']
           PF2Magic.update_magic(enactor, charclass, magic_stats, client)
         end
 
-        # Handle reagents if necessary. 
+        # Handle reagents if necessary.
           reagents = self.feat_details['reagents']
 
           if reagents
             Pf2e.update_reagents(char, reagents)
           end
 
-        # Does this feat leave you with something else to assign? 
+        # Does this feat leave you with something else to assign?
 
         cascade = self.feat_details['assign']
 
@@ -173,7 +173,7 @@ module AresMUSH
       end
 
     end
-    
+
   end
-  
+
 end
