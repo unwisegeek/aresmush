a1f28632880b8eae6802c83c90b01586bedaab5d
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-03-02 14:33:51 -0800

    order of operations

--- plugins/pf2e/models/hitpoints.rb
+++ plugins/pf2e/models/hitpoints.rb
@@ -24,7 +24,7 @@ module AresMUSH
 
       current = get_current_hp(char)
       max = get_max_hp(char)
-      percent = max.zero? ? 0 : ((max - current) / max) * 100.floor
+      percent = max.zero? ? 0 : (((max - current) / max) * 100).floor
       hp_color = "%xg" if percent > 75
       hp_color = "%xc" if percent.between?(50,75)
       hp_color = "%xy" if percent.between?(25,50)
