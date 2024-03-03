b99ca96516120fd7a40bd5855932d2ce20070738
Admin <admin@159.203.105.162>
2024-03-03 06:54:16 +0000

    Spellbook command

---
+++ plugins/pf2emagic/templates/spellbook.rb
@@ -0,0 +1,36 @@
+module AresMUSH
+    module Pf2emagic
+  
+      class PF2SpellbookTemplate < ErbTemplateRenderer
+        include CommonTemplateFields
+  
+        attr_accessor :char, :spellbook, :client
+  
+        def initialize(char, spellbook, client)
+          @char = char
+          @spellbook = spellbook
+          @client = client
+          @charclass = spellbook.keys[0] # There can be only one!
+  
+          super File.dirname(__FILE__) + "/spellbook.erb"
+        end
+  
+        def title
+          t('pf2emagic.spellbook_title', :charclass => @charclass, :name => @char.name)
+        end
+  
+        def spellbook_list
+          list = []
+          
+          @spellbook[@charclass].each_pair do |spell_level, spell_list|
+            spells = spellbook[@charclass][spell_level].join(", ")
+            list << "#{item_color}#{spell_level}:%xn #{spells}"
+          end
+  
+          list
+        end
+  
+      end
+    end
+  end
+  
