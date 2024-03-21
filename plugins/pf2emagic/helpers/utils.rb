24a000e2f87ee05fd1e816cb19c23ab66c70d0cd
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-03-20 20:49:43 -0700

    Searching is weird

--- plugins/pf2emagic/helpers/utils.rb
+++ plugins/pf2emagic/helpers/utils.rb
@@ -143,7 +143,7 @@ module AresMUSH
       when 'name'
         match = spell_info.select { |k,v| k.upcase.match? term.upcase }
       when 'traits'
-        match = spell_info.select { |k,v| (v['traits'].include? term.downcase) }
+        match = spell_info.select { |k,v| v['traits'].include? term.downcase }
       when 'level'
         # Invalid operator defaults to ==.
         case operator
@@ -157,13 +157,13 @@ module AresMUSH
       when 'tradition'
         match = spell_info.select { |k,v| v['tradition'] && (v['tradition'].include? term.downcase) }
       when 'school'
-        match = spell_info.select { |k,v| (v['school']&.include? term.capitalize) && v['tradition'] }
+        match = spell_info.select { |k,v| v['school']&.include?(term.capitalize) }
       when 'bloodline'
-        match = spell_info.select { |k,v| v['bloodline']&.include? term.downcase }
+        match = spell_info.select { |k,v| v['bloodline']&.include?(term.downcase) }
       when 'cast'
-        match = spell_info.select { |k,v| (v['cast']&.include? term.downcase) }
+        match = spell_info.select { |k,v| v['cast']&.include? term.downcase }
       when 'description', 'desc', 'effect'
-        match = spell_info.select { |k,v| (v['effect'].upcase.match? term.upcase) }
+        match = spell_info.select { |k,v| v['effect'].upcase.match? term.upcase }
       end
 
       match.keys
