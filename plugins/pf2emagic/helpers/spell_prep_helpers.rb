627b195127a33759970dd1dab1e2b3441a059390
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-03-23 13:59:53 -0700

    Poik.

--- plugins/pf2emagic/helpers/spell_prep_helpers.rb
+++ plugins/pf2emagic/helpers/spell_prep_helpers.rb
@@ -58,6 +58,9 @@ module AresMUSH
 
       spell_trad = spell_details['tradition']
 
+      client.emit spell_trad
+      client.emit tradition
+
       return t('pf2emagic.cant_prepare_trad', :cc => cc) unless spell_trad.include? tradition[1].downcase
 
       spell_level = spell_details['base_level']
