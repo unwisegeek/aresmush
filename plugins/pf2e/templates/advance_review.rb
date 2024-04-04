622e91058be89c142c278bc683d762d809058a89
Andrea Bigelow <abigelow@hunterfiber.com>
2024-04-04 12:53:32 -0700

    Debugging advance/review 1

--- plugins/pf2e/templates/advance_review.rb
+++ plugins/pf2e/templates/advance_review.rb
@@ -34,14 +34,14 @@ module AresMUSH
 
         hash.each_pair do |key, value|
           # Process according to the data type of the key.
-          heading = key.gsub("charclass", "class").split.each {|word| word.capitalize}.join
+          heading = key.gsub("charclass", "class").split("_").each {|word| word.capitalize}.join(" ")
 
           if value.is_a? Array
             list << "#{item_color}#{heading}:%xn #{value.sort.join(", ")}" unless value.empty?
           elsif value.is_a? Hash
             sublist = []
             value.each_pair do |subkey, subvalue|
-              subheading = subkey.gsub("charclass", "class").split("_").each {|word| word.capitalize}.join
+              subheading = subkey.gsub("charclass", "class").split("_").each {|word| word.capitalize}.join(" ")
               if subvalue.is_a? Array
                 sublist << "%b%b#{item_color}#{subheading}:%xn #{subvalue.sort.join(", ")}"
               # elsif subvalue.is_a? Hash
