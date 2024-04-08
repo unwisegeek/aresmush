da12d864886ed9badf0d37153667189ea57314d1
Andrea Bigelow <abigelow@hunterfiber.com>
2024-04-08 15:25:51 -0700

    FIxes to use command

--- plugins/pf2egear/commands/use.rb
+++ plugins/pf2egear/commands/use.rb
@@ -59,8 +59,9 @@ module AresMUSH
         # Is this a usable item?
 
         use = item.use
+        # Consumables are always usable. Check other categories.
 
-        if use.empty?
+        if use.empty? && !(self.category.match? 'consumable')
           client.emit_failure t('pf2egear.not_usable')
           return
         end
@@ -79,11 +80,8 @@ module AresMUSH
             client.emit_failure t('pf2egear.cannot_use_now', :action => 'invested')
             return
           end
-        end
-
-        # Consumables get their own, much simpler, handling.
-
-        if item.instance_of? PF2Consumable
+        else
+          # Consumables get their own, much simpler, handling.
 
           template = PF2UseItemTemplate.new(enactor, item, {})
           message = template.render
@@ -108,7 +106,7 @@ module AresMUSH
 
         # Some items have more than one use. Expect a valid self.use_option if this is the case.
 
-        uses = use.keys
+        uses = use.keys || []
 
         if uses.size > 1 && !self.use_option
           client.emit_failure t('pf2egear.needs_use_option', :options => uses.keys.sort.join(", "))
